74c0746077fe9c178ea92109b075b7b6f4d56ae4
XopK <dmahmutov12@gmail.com>
2024-01-23 12:07:38 +0500

    победа

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('surname');
-            $table->string('patronymic');
+            $table->string('surname')->nullable();
+            $table->string('patronymic')->nullable();
             $table->string('phone');
             $table->string('login')->unique();
             $table->string('email')->unique();
